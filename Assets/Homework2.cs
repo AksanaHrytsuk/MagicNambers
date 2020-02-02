cda4f7fc0c541fbd17cda552358717d614989427
AksanaHrytsuk <oksana@redeyes.homenetwork>
2020-02-02 04:56:10 +0300

    final changes

---
+++ Assets/Homework2.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Homework2 : MonoBehaviour
+{
+  // Start is called before the first frame update
+  int apples = 0;
+  int count = 0;
+  int final_apples = 50;
+  void Start()
+  {
+    Debug.Log("Welcome to the game!");
+    Debug.Log("Enter numbers from 1 to 9");
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Alpha1))
+    {
+      Summ(1);
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha2))
+    {
+      Summ(2);
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha3))
+    {
+      Summ(3);
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha4))
+    {
+      Summ(4);
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha5))
+    {
+      Summ(5);
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha6))
+    {
+      Summ(6);
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha7))
+    {
+      Summ(7);
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha8))
+    {
+      Summ(8);
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha9))
+    {
+      Summ(9);
+    }
+  }
+  void Summ(int number)
+  {
+    if (apples < final_apples)
+    {
+      apples = apples + number;
+      Debug.Log(number);
+      Debug.Log("I have: " + apples + " apples");
+      count++;
+    }
+    else
+    {
+      Debug.Log("There is enough apples.");
+      Debug.Log(count + " steps");
+    }
+  }
+}

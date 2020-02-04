da93e1a89f27242d54cb5d5fea6631f5843ba5fd
Студент <st@fungi.lan>
2020-02-04 21:18:16 +0300

    fs

---
+++ Assets/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Player playerVar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerVar.playerName = "Bad player";
+        Debug.Log("Hello" + playerVar.playerName + "I am your enemy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string playerName;
+    public Enemy enemyVar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, my name is " + playerName);
+        Debug.Log(enemyVar);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

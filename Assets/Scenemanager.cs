dfec664f58e95e2317486ae5446933e534f1ec7c
AksanaHrytsuk <30596314+AksanaHrytsuk@users.noreply.github.com>
2020-02-11 21:24:07 +0300

    Added button

---
+++ Assets/Scenemanager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenemanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void Click()
+    {
+        Debug.Log("Button pressed");
+    }
+}

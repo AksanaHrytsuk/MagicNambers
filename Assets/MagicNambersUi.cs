48ddb66b39ade6c6d16b61445dd4b5fd91a22e63
AksanaHrytsuk <30596314+AksanaHrytsuk@users.noreply.github.com>
2020-02-13 21:18:10 +0300

    TextMashProUGUI

---
+++ Assets/MagicNambersUi.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class MagicNambersUi : MonoBehaviour
+{
+    public int guess;
+    public int minNumber = 1;
+    public int maxNumber = 1000;
+    public Text textguess;
+    public TextMeshProUGUI textCount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        guess = Random.Range(minNumber, maxNumber + 1);
+        guess = (minNumber + maxNumber) / 2;
+        textguess.text = "Ваше число " + guess.ToString() + "?";
+        textCount.text = "Ololo";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
--- Assets/ScenLoader.cs
+++ Assets/ScenLoader.cs
@@ -6,15 +6,19 @@ using UnityEngine.SceneManagement;
 public class ScenLoader : MonoBehaviour
 {
     public void LoadNextLevel(int LevelIndex)
-{
+    {
         int activeSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(activeSceneIndex + 1);
 
-}
+    }
 
     public void firstscene()
     {
-    SceneManager.LoadScene(0);
+        SceneManager.LoadScene(0);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
 
